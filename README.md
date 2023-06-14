85757f77e13d1073d5ebeb733d7aa9051903de60
Jakub Łaba <77542257+jakublaba@users.noreply.github.com>
2023-06-14 19:27:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# GTFS feed parser
+
+# Test data
+
+- `sample-feed-1.zip` - from [gtfs.org](https://gtfs.org)
---
+++ src/main/java/com/krabelard/Main.java
@@ -0,0 +1,7 @@
+package com.krabelard;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

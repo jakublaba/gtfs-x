3803cd593a71877ba5cd584d350f1c3206fac2d7
jakublaba <jakub.maciej.laba@gmail.com>
2023-06-20 01:25:22 +0200

    Add sample gtfs entity javadocs

--- src/main/java/com/krabelard/model/required/Agency.java
+++ src/main/java/com/krabelard/model/required/Agency.java
@@ -5,25 +5,75 @@ import lombok.Value;
 
 import java.util.Optional;
 
-// Required
+/**
+ * Maps an entry from required <code>agencies.txt</code> file.
+ * <p>
+ * For full specification, visit <a href="https://gtfs.org/schedule/reference/#agencytxt">the gtfs reference</a>
+ */
 @Value
 @Builder
 public class Agency {
-    Long id;            // Conditionally required
-    String name;        // Required
-    String url;         // Required
-    String timezone;    // Required
-    String language;    // Optional
-    String phoneNumber; // Optional
-    String fareUrl;     // Optional
-    String email;       // Optional
+    /**
+     * <code>agency_id</code>
+     * <p>
+     * <b>Conditionally required</b>
+     * <p>
+     * Required when the dataset contains data for multiple transit agencies.
+     */
+    Long id;
+    /**
+     * <code>agency_name</code>
+     * <p>
+     * <b>Required</b>
+     */
+    String name;
+    /**
+     * <code>agency_url</code>
+     * <p>
+     * <b>Required</b>
+     */
+    String url;
+    /**
+     * <code>agency_timezone</code>
+     * <p>
+     * <b>Required</b>
+     * <p>
+     * If multiple agencies are specified in the dataset, each must have the same timezone.
+     */
+    String timezone;
+    /**
+     * <code>agency_lang</code>
+     * <p>
+     * <b>Optional</b>
+     */
+    String language;
+    /**
+     * <code>agency_phone</code>
+     * <p>
+     * <b>Optional</b>
+     * <p>
+     * May contain punctuation marks to group the digits of the number.
+     */
+    String phone;
+    /**
+     * <code>agency_fare_url</code>
+     * <p>
+     * <b>Optional</b>
+     */
+    String fareUrl;
+    /**
+     * <code>agency_email</code>
+     * <p>
+     * <b>Optional</b>
+     */
+    String email;
 
     public Optional<String> getLanguage() {
         return Optional.ofNullable(language);
     }
 
-    public Optional<String> getPhoneNumber() {
-        return Optional.ofNullable(phoneNumber);
+    public Optional<String> getPhone() {
+        return Optional.ofNullable(phone);
     }
 
     public Optional<String> getFareUrl() {

08e78e0fdaca7fdcc9957f812fa43b0e2f5105cc
jakublaba <jakub.maciej.laba@gmail.com>
2023-07-09 16:14:52 +0200

    Add javadocs to optional GTFS-Fares V1 entities

--- src/main/java/com/krabelard/model/enums/TransfersAllowed.java
+++ src/main/java/com/krabelard/model/enums/TransfersAllowed.java
@@ -1,13 +1,25 @@
 package com.krabelard.model.enums;
 
+import com.krabelard.model.optional.gtfs_fares_v1.FareAttribute;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * Representation for {@link FareAttribute#transfers}.
+ * <br>
+ * Valid options are:
+ * <li><code>0</code> - No transfers permitted on this fare.</li>
+ * <li><code>1</code> - Riders may transfer once.</li>
+ * <li><code>2</code> - Riders may transfer twice.</li>
+ * <li>empty - Unlimited transfers are permitted</li>
+ */
 @RequiredArgsConstructor
+@Getter
 public enum TransfersAllowed {
     NotAllowed(0),
     Once(1),
     Twice(2),
-    // Note - gtfs specification says that empty field means unlimited transfers, so this has to be mapped on library level
+    // TODO - gtfs specification says that empty field means unlimited transfers, so this has to be mapped on library level
     Unlimited(3);
 
     private final int transfersAllowed;

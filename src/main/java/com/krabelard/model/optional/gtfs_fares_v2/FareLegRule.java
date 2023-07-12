4df834c442fec784f4a71016ab244b4ec06e2775
jakublaba <jakub.maciej.laba@gmail.com>
2023-07-12 11:49:35 +0200

    Add javadocs to optional GTFS-Fares V2 entities

--- src/main/java/com/krabelard/model/optional/gtfs_fares_v2/FareLegRule.java
+++ src/main/java/com/krabelard/model/optional/gtfs_fares_v2/FareLegRule.java
@@ -5,16 +5,74 @@ import lombok.Value;
 
 import java.util.Optional;
 
-// Optional
-// Associate with GTFS-Fares V2
+/**
+ * Maps an entry from optional <code>fare_leg_rules.txt</code> file.
+ * <p>
+ * <b>Versions</b>
+ * <br>
+ * There are two modelling options for describing fares. GTFS-Fares V1 is the legacy option for describing minimal fare information.
+ * GTFS-Fares V2 is an updated method that allows for a more detailed account of an agency's fare structure.
+ * Both are allowed to be present in a dataset, but only one method should be used by a data consumer for a given dataset.
+ * It is recommended that GTFS-Fares V2 takes precedence over GTFS-Fares V1.
+ * <p>
+ * For full specification and details about processing leg costs, visit <a href="https://gtfs.org/schedule/reference/#fare_leg_rulestxt">the gtfs reference</a>
+ */
 @Value
 @Builder
 public class FareLegRule {
-    Long legGroupId;    // Optional
-    Long networkId;     // Optional
-    Long fromAreaId;    // Optional
-    Long toAreaId;      // Optional
-    long fareProductId; // Required
+    /**
+     * <code>leg_group_id</code>
+     * <p>
+     * <b>Optional</b>
+     * <p>
+     * Identifies a group of entries.
+     * <br>
+     * Used to describe a fare transfer rules between {@link FareTransferRule#fromLegGroupId} and {@link FareTransferRule#toLegGroupId}.
+     * Multiple {@link FareLegRule} entities may belong to the same {@link FareLegRule#legGroupId}.
+     * The same {@link FareLegRule} entity must not belong belong to multiple {@link FareLegRule#legGroupId}.
+     */
+    Long legGroupId;
+    /**
+     * <code>network_id</code>
+     * <p>
+     * <b>Optional</b>
+     * <p>
+     * Identifies a route network that applies for the fare leg rule.
+     * <br>
+     * If there are no matching {@link FareLegRule#networkId} values to the <code>networkId</code> being filtered,
+     * empty {@link FareLegRule#networkId} will be matched by default.
+     */
+    Long networkId;
+    /**
+     * <code>from_area_id</code>
+     * <p>
+     * <b>Optional</b>
+     * <p>
+     * Identifies a departure area.
+     * <br>
+     * If there are no matching {@link FareLegRule#fromAreaId} values to the <code>fromAreaId</code> being filtered,
+     * empty {@link FareLegRule#fromAreaId} will be matched by default.
+     */
+    Long fromAreaId;
+    /**
+     * <code>to_area_id</code>
+     * <p>
+     * <b>Optional</b>
+     * <p>
+     * Identifies an arrival area.
+     * <br>
+     * If there are not matching {@link FareLegRule#toAreaId} values to the <code>toAreaId</code> being filtered,
+     * empty {@link FareLegRule#toAreaId} will be matched by default.
+     */
+    Long toAreaId;
+    /**
+     * <code>fare_product_id</code>
+     * <p>
+     * <b>Required</b>
+     * <p>
+     * The fare product required to travel the leg.
+     */
+    long fareProductId;
 
     public Optional<Long> getLegGroupId() {
         return Optional.ofNullable(legGroupId);
--- src/main/java/com/krabelard/model/optional/gtfs_fares_v2/FareMedia.java
+++ src/main/java/com/krabelard/model/optional/gtfs_fares_v2/FareMedia.java
@@ -6,14 +6,46 @@ import lombok.Value;
 
 import java.util.Optional;
 
-// Optional
-// Associated with GTFS-Fares V2
+/**
+ * Maps an entry from optional <code>fare_leg_rules.txt</code> file.
+ * <p>
+ * <b>Versions</b>
+ * <br>
+ * There are two modelling options for describing fares. GTFS-Fares V1 is the legacy option for describing minimal fare information.
+ * GTFS-Fares V2 is an updated method that allows for a more detailed account of an agency's fare structure.
+ * Both are allowed to be present in a dataset, but only one method should be used by a data consumer for a given dataset.
+ * It is recommended that GTFS-Fares V2 takes precedence over GTFS-Fares V1.
+ * <p>
+ * For full specification, visit <a href="https://gtfs.org/schedule/reference/#fare_mediatxt">the gtfs reference</a>
+ */
 @Value
 @Builder
 public class FareMedia {
-    long id;                    // Required
-    String name;                // Optional
-    FareMediaType mediaType;    // Required
+    /**
+     * <code>fare_media_id</code>
+     * <p>
+     * <b>Required</b>
+     * <p>
+     * Identifies a fare media.
+     */
+    long id;
+    /**
+     * <code>fare_media_name</code>
+     * <p>
+     * <b>Optional</b>
+     * <p>
+     * Name of the fare media. For fare media which are transit cards ({@link FareMediaType#cEMV}) or mobile apps ({@link FareMediaType#MobileApp}),
+     * this field should be included and should match the rider-facing name used by the organizations delivering them.
+     */
+    String name;
+    /**
+     * <code>fare_media_type</code>
+     * <p>
+     * <b>Required</b>
+     * <p>
+     * Type of fare media.
+     */
+    FareMediaType mediaType;
 
     public Optional<String> getName() {
         return Optional.ofNullable(name);

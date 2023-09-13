f7338ff05606912bceaacf55f3f53bf61b8f19e2
jakublaba <jakub.maciej.laba@gmail.com>
2023-09-13 13:07:23 +0200

    Implement FareMediaParser

--- src/main/java/com/krabelard/parsers/optional/gtfs_fares_v2/FareMediaParser.java
+++ src/main/java/com/krabelard/parsers/optional/gtfs_fares_v2/FareMediaParser.java
@@ -1,6 +1,7 @@
 package com.krabelard.parsers.optional.gtfs_fares_v2;
 
-import com.krabelard.model.optional.gtfs_fares_v2.FareLegRule;
+import com.krabelard.model.enums.FareMediaType;
+import com.krabelard.model.optional.gtfs_fares_v2.FareMedia;
 import com.krabelard.parsers.CsvHeaders;
 import com.krabelard.parsers.GtfsCsvParser;
 import com.krabelard.parsers.GtfsParsingException;
@@ -14,20 +15,20 @@ import java.nio.file.NoSuchFileException;
 import java.util.Collection;
 
 @Slf4j
-public class FareLegRuleParser implements GtfsCsvParser<FareLegRule> {
-    private static final String GTFS_FILE_NAME = "fare_leg_rules.txt";
+public class FareMediaParser implements GtfsCsvParser<FareMedia> {
+    private static final String GTFS_FILE_NAME = "fare_media.txt";
     private final String csv;
 
-    private FareLegRuleParser(String dir) {
+    private FareMediaParser(String dir) {
         this.csv = dir + "/" + GTFS_FILE_NAME;
     }
 
-    public static FareLegRuleParser of(String dir) {
-        return new FareLegRuleParser(dir);
+    public static FareMediaParser of(String dir) {
+        return new FareMediaParser(dir);
     }
 
     @Override
-    public Collection<FareLegRule> parse() throws GtfsParsingException, NoSuchFileException {
+    public Collection<FareMedia> parse() throws GtfsParsingException, NoSuchFileException {
         try {
             log.info("Parsing {}", csv);
             var headers = CsvUtil.headersAsStrings(Headers.values());
@@ -35,12 +36,13 @@ public class FareLegRuleParser implements GtfsCsvParser<FareLegRule> {
                     .stream()
                     .map(r -> {
                         var values = CsvUtil.extractValues(r, headers);
-                        return FareLegRule.builder()
-                                .legGroupId(values.get(Headers.LegGroupId.value))
-                                .networkId(values.get(Headers.NetworkId.value))
-                                .fromAreaId(values.get(Headers.FromAreaId.value))
-                                .toAreaId(values.get(Headers.ToAreaId.value))
-                                .fareProductId(values.get(Headers.FareProductId.value))
+                        return FareMedia.builder()
+                                .id(values.get(Headers.FareMediaId.value))
+                                .name(values.get(Headers.FareMediaName.value))
+                                .mediaType(CsvUtil.parseEnum(
+                                        FareMediaType.class,
+                                        Integer.parseInt(values.get(Headers.FareMediaType.value))
+                                ))
                                 .build();
                     })
                     .toList();
@@ -54,13 +56,9 @@ public class FareLegRuleParser implements GtfsCsvParser<FareLegRule> {
     @RequiredArgsConstructor
     @Getter
     private enum Headers implements CsvHeaders {
-        LegGroupId("leg_group_id"),
-        NetworkId("network_id"),
-        FromAreaId("from_area_id"),
-        ToAreaId("to_area_id"),
-        FromTimeframeGroupId("from_timeframe_group_id"),
-        ToTimeframeGroupId("to_timeframe_group_id"),
-        FareProductId("fare_product_id");
+        FareMediaId("fare_media_id"),
+        FareMediaName("fare_media_name"),
+        FareMediaType("fare_media_type");
 
         private final String value;
     }

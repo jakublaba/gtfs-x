5c0dc31db461b9a8be8a5ddcb52cb11f2c2f9457
Jakub Łaba <77542257+jakublaba@users.noreply.github.com>
2023-10-19 01:10:35 +0200

    Add tests for parsers (#26)
    * Refactor ZipHandlerTest
    
    * Fix optional field parsing (#16)
    
    * Add test data
    
    * Implement unit tests for required entity parsers
    
    * Update test data
    
    * Update existing tests for new test data
    
    * Implement test for optional GTFS entity parsers and fix detected bugs
    
    * Remove main from one of the parsers
    
    * Move ZipHandlerTest to correct package
    
    * Add tests for parsers

---
+++ src/test/java/com/krabelard/parsers/optional/TranslationParserTest.java
@@ -0,0 +1,36 @@
+package com.krabelard.parsers.optional;
+
+import com.krabelard.model.enums.TableName;
+import com.krabelard.model.optional.Translation;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import testutil.TestConstants;
+
+import java.nio.file.NoSuchFileException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class TranslationParserTest {
+    @Test
+    @SneakyThrows
+    void shouldParseCsvCorrectly() {
+        var translations = TranslationParser.of(TestConstants.FEED_DIR).parse();
+        var expectedSize = 1;
+        var expectedTranslation = Translation.builder()
+                .tableName(TableName.STOPS)
+                .fieldName("stop_name")
+                .recordId("S8815040")
+                .language("nl")
+                .translation("Brussel-West")
+                .build();
+        assertEquals(expectedSize, translations.size());
+        assertEquals(expectedTranslation, translations.get(0));
+    }
+
+    @Test
+    void shouldThrowWhenFileNotPresent() {
+        var parser = TranslationParser.of(TestConstants.EMPTY_DIR);
+        assertThrows(NoSuchFileException.class, parser::parse);
+    }
+}
